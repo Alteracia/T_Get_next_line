9ec2c7c7dddabff2036fa2a85c97cfc1d9f716e4
Abbie Calandra <acalandr@oa-l4.21-school.ru>
2019-04-18 15:32:57 +0300

    norm_speed

--- gcc.sh
+++ gcc.sh
@@ -1,13 +1,17 @@
 
 make re --directory ~/$1/libft
 gcc -Wall -Wextra -Werror -I ~/$1/libft -o ~/$1/get_next_line.o -c ~/$1/get_next_line.c
-gcc -Wall -Wextra -Werror -I ~/$1/ -I ~/$1/libft -o main.o -c main.c
-gcc -Wall -Wextra -Werror -I ~/$1/ -I ~/$1/libft -o ft_errors.o -c ft_errors.c
-gcc -O0 -g -o test_gnl main.o ft_errors.o ~/$1/get_next_line.o -I ~/$1/libft -L ~/$1/libft/ -lft
-./test_gnl onestring
-./test_gnl twostrings
-./test_gnl emptyline
-./test_gnl errors
-./test_gnl onestring twostrings
-rm test_gnl
+gcc -Wall -Wextra -Werror -I ~/$1/ -I ~/$1/libft -c *.c
+gcc -O0 -g -o test *.o ~/$1/get_next_line.o -I ~/$1/libft -L ~/$1/libft/ -lft
+./test onestring
+./test twostrings
+./test emptyline
+#./test_gnl errors
+./test onestring twostrings
+./test -m first second third
+rm *.o
+rm test
 make --directory ~/$1/libft fclean
+gcc -Wall -Wextra -Werror -g -o test -I ~/$1/libft -I ~/$1 ~/$1/libft/*.c ~/$1/*.c *.c
+~/Applications/valgrind/bin/valgrind --leak-check=full ./test -mh errors errors_1 errors_2
+rm test
--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int		read_file(int c, char **arg)
 		ft_putstr("\ncontent of file \"");
 		ft_putstr(arg[c]);
 		ft_putstr("\"\n");
-	}	
+	}
 	while (get_next_line(op, &line))
 	{
 		ft_putstr(line);
@@ -48,28 +48,106 @@ int		read_file(int c, char **arg)
 		err = errno;
 		if (err != 0)
 			return (err_message(err, arg[0], arg[c]));
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
+int		read_multiple(int c, char **arg)
+{
+	int		op[3];
+	char	*line;
+	int		err;
+	int 	i = 0;
+	
+	line = NULL;
+	while (i < 3)
+	{
+		errno = 0;
+		op[i] = open(arg[i + 2], O_RDONLY);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));
+		i++;
+	}
+	i = 0;
+	ft_putstr("\nMultiple read:\n");
+	while (get_next_line(op[i], &line))
+	{
+		ft_putstr(line);
+		ft_putstr(" ");
+		i++;
+		if (i > 2)
+			i = 0;
+	}
+	ft_putstr("\n");
+	i = 0;
+	while (i < 3)
+	{
+		close(op[i]);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));
+		return (1);
+		i++;
+	}
+	return (1);
+}
+
+int		read_mult_empty(int c, char **arg)
+{
+	int		op[3];
+	char	*line;
+	int		err;
+	int 	i = 0;
+	
+	line = NULL;
+	while (i < 3)
+	{
+		errno = 0;
+		op[i] = open(arg[i + 2], O_RDONLY);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));
+		i++;
+	}
+	i = 0;	
+	while (get_next_line(op[i], &line))
+	{		
+		i++;
+		if (i > 2)
+			i = 0;
+	}
+	i = 0;
+	while (i < 3)
+	{
+		close(op[i]);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));
+		return (1);
+		i++;
+	}
+	return (1);
+}
 
 int main(int c, char **argv)
 {
 	int i;
-	/*int		op = -1;	
-	char	*line;*/
-
-	if (c > 1)
+	
+	if (c > 0)
 	{
 		i = 1;
 		if (c == 1)
-			while (c)
-				read_file(0, argv);
+			while (c)			
+				read_file(0, argv);	
+		else if (strcmp(argv[1], "-m") == 0)
+			read_multiple(c, argv);
+		else if (strcmp(argv[1], "-mh") == 0)
+			read_mult_empty(c, argv);
 		else
 			while (i < c)
 				read_file(i++, argv);
-	}
-	/*line = NULL;*/
-	/*get_next_line(op, &line);*/
+	}	
 	return (0);
 }
