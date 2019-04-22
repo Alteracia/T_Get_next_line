d804e59ad6eb095358132220fa465e169704d220
Abbie Calandra <acalandr@oa-l4.21-school.ru>
2019-04-22 20:38:04 +0300

    norm2

--- gcc.sh
+++ gcc.sh
@@ -9,8 +9,11 @@ gcc -O0 -g -o test *.o ~/$1/get_next_line.o -I ~/$1/libft -L ~/$1/libft/ -lft
 ./test errors
 ./test onestring twostrings emptyline
 ./test -m first second third
+date +%s
+./test -h Voyna-i-mir.txt
+date +%s
 rm ~/$1/*.o *.o test
-make --directory ~/$1/libft fclean
+#make --directory ~/$1/libft fclean
 #gcc -Wall -Wextra -Werror -g -o test -I ~/$1/libft -I ~/$1 ~/$1/libft/*.c ~/$1/*.c *.c
 #~/Applications/valgrind/bin/valgrind --leak-check=full ./test -mh errors errors_1 errors_2
 #rm test
--- main.c
+++ main.c
@@ -54,6 +54,38 @@ int		read_file(int c, char **arg)
 	return (1);
 }
 
+int		read_file_hide(int c, char **arg)
+{
+	int		op = 0;	
+	char	*line;
+	int		err;
+	int i;
+	
+	i = 0;
+	line = NULL;
+	if (c > 0)
+	{
+		errno = 0;
+		op = open(arg[c], O_RDONLY);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));		
+	}
+	while (get_next_line(op, &line))
+	{
+		i++;
+	}
+	if(c > 0)
+	{
+		close(op);
+		err = errno;
+		if (err != 0)
+			return (err_message(err, arg[0], arg[c]));
+		return (0);
+	}
+	return (1);
+}
+
 int		read_multiple(int c, char **arg)
 {
 	int		op[3];
@@ -136,56 +168,7 @@ int		read_mult_empty(int c, char **arg)
 
 int main(int c, char **argv)
 {
-	int i;
-
-	/*char *temp;
-	char *temp2;*/
-	/*t_list *list;*/
-		
-	/*temp = ft_strnew(5);*/
-	/*temp = ft_strcpy(temp, "five ");*/
-
-	/*temp2 = strcat(NULL, temp);
-	ft_putstr(temp2);*/
-
-	/*list = split_for_lines(temp, 42);*/
-
-	/*list = ft_lstnew(NULL, 0);
-	list->content = split_for_lines(temp, 42);
-	list->content_size = 4;
-	ft_strdel(&temp);
-	if (!seach_fd_line(&list, &temp, list->content_size))
-	{
-		temp2 = ft_strnew(5);
-		temp2 = ft_strcpy(temp2, "and 5");
-		if (temp)
-			temp = ft_strncat(temp, temp2, 6);*/
-		/*ft_putstr(temp);*/
-		/*list = ft_lstnew(NULL, 0);
-		list->content = split_for_lines(temp, 42);
-		list->content_size = 4;
-		ft_strdel(&temp);
-		ft_strdel(&temp2);
-	}
-	if (!seach_fd_line(&list, &temp, list->content_size))
-	{
-		ft_putstr(temp);
-	}*/
-	/*ft_strdel(&temp);
-	if (seach_fd_line(&list, &temp, list->content_size))
-		ft_putstr(temp);
-	ft_strdel(&temp);
-	if (!seach_fd_line(&list, &temp, list->content_size))
-		ft_putstr(temp);*/
-
-	/*ft_print_list(list->content);*/
-	
-	/*ft_strdel(&temp);
-	temp = (char*)list->content;
-	temp = ft_strnew(13);
-	temp = ft_strcpy(temp, (char*)list->content);
-	temp = ft_strcat(temp, "four");
-	ft_putstr(temp);*/
+	int i;	
 
 	if (c > 0)
 	{
@@ -197,6 +180,8 @@ int main(int c, char **argv)
 			read_multiple(c, argv);
 		else if (strcmp(argv[1], "-mh") == 0)
 			read_mult_empty(c, argv);
+		else if (strcmp(argv[1], "-h") == 0)
+			read_file_hide(2, argv);
 		else
 			while (i < c)
 				read_file(i++, argv);
